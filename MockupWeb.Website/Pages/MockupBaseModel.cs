3bb66db4000e21843322be1f032b35ed4553d396
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2017-09-10 12:58:49 -0700

    initial add for web project

---
+++ MockupWeb.Website/Pages/MockupBaseModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MockupWeb.Website.Pages
+{
+    public class MockupBaseModel : PageModel {
+        public MockupBaseModel(IHostingEnvironment hostingEnvironment) : base() {
+            _hostingEnvironment = hostingEnvironment;
+            _webRoot = _hostingEnvironment.WebRootPath;
+            SetMockupRoot("mockups");
+        }
+        private IHostingEnvironment _hostingEnvironment;
+        public string _webRoot { get; set; }
+        public string MockupRoot { get; set; }
+        public void SetMockupRoot(string reldirpath) {
+            MockupRoot = Path.Combine(_webRoot, reldirpath);
+        }
+    }
+}
